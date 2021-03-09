e99f176bd9dfaf4882b8b466aa1c01372ec9973e
Victor Munduruca <victormunduruca@gmail.com>
2021-03-08 22:33:31 -0300

    testes parciais de strings funcionando

--- lex/states.py
+++ lex/states.py
@@ -11,7 +11,26 @@ class States(Enum):
 
     rel = 7
     rel_equal = 8
+
     exclamation = 9
 
     log_incomplete = 10
     log_complete = 11
+
+    art_plus = 12
+    art_minus = 13
+    art_complete = 14
+    slash = 15
+
+    delimiter = 16
+
+    com_line = 17
+    com_line_complete = 18
+    com_block = 19
+    com_block_after_asterisk = 20
+    com_block_complete = 21
+
+    string = 22 
+    string_escape = 23
+    string_escape_quote = 24
+    string_final = 25
--- lex/test.py
+++ lex/test.py
@@ -2,6 +2,17 @@ import re
 import util
 from states import *
 
+delimiters = [
+    ';',
+    ',',
+    '(',
+    ')',
+    '{',
+    '}',
+    '[',
+    ']',
+    '.'
+]
 
 def isdigit(x):
     return re.match(r'\d', x, re.ASCII) is not None
@@ -12,7 +23,7 @@ def isletter(x):
 
 
 def issymbol(x):
-    return (ord(x) > 31) and (ord(x) < 127)
+    return (ord(x) > 31) and (ord(x) < 127) and not (ord(x) == 34)
 
 # def isdelimiter(x):
 #     return x is in delimiters
@@ -23,12 +34,24 @@ def temp_transitions(state, char):
             return States.num 
         elif char.isalpha():
             return States.ide
+        elif char in delimiters:
+            return States.delimiter
         elif char == '>' or char == '<' or char == '=':
             return States.rel
         elif char == '!':
             return States.exclamation
         elif char == '&' or char == '|':
             return States.log_incomplete
+        elif char == '+':
+            return States.art_plus
+        elif char == '-':
+            return States.art_minus
+        elif char == '*':
+            return States.art_complete
+        elif char == '/':
+            return States.slash
+        elif char == "\"":
+            return States.string
 
 
     elif state == States.num:
@@ -58,11 +81,68 @@ def temp_transitions(state, char):
         if char == '&' or char == '|':
             return States.log_complete
 
+    elif state == States.art_plus:
+        if char == '+':
+            return States.art_complete
+    elif state == States.art_minus:
+        if char == '-':
+            return States.art_complete
+
+    elif  state == States.slash:
+        if char == '/':
+            return States.com_line
+        elif char == '*':
+            return States.com_block
+
+    elif state == States.com_line:
+        if char == '\n':
+            return States.com_line_complete
+        return States.com_line
+    elif state == States.com_block:
+        if char == '*':
+            return States.com_block_after_asterisk
+        return States.com_block
+    elif state == States.com_block_after_asterisk:
+        if char == '*':
+            return States.com_block_after_asterisk
+        elif char == '/':
+            return States.com_block_complete
+        return States.com_block_complete
+
+    #String 
+    elif state == States.string:
+        if char == '\\':
+            return States.string_escape
+        if char.isalpha() or char.isdigit() or issymbol(char):
+            return States.string
+        elif char == "\"":
+            return States.string_final
+    elif state == States.string_escape:
+        if char == "\"":
+            return States.string_escape_quote
+        elif char == "\\":
+            return States.string_escape
+        elif char.isalpha() or char.isdigit() or issymbol(char):
+            return States.string
+    elif state == States.string_escape_quote:
+        if char == "\"":
+            return States.string_final
+        elif char == "\\":
+            return States.string_escape
+        elif char.isalpha() or char.isdigit() or issymbol(char):
+            return States.string
+
+
+
+
+
     return States.invalid_state #TODO Save buffer logic
 
+FILE = open('entrada/entrada1.txt', 'r')
+input_string = FILE.read()
+fsm = util.Fsm([0], States.start, [States.num, States.num_after_dot, States.rel_equal, States.rel, States.exclamation, States.ide, States.log_complete, States.art_complete, States.art_plus, States.art_minus, States.slash, States.delimiter, States.com_line_complete, States.com_block_complete, States.string_final], temp_transitions)
 
-fsm = util.Fsm([0], States.start, [States.num, States.num_after_dot, States.rel_equal, States.rel, States.exclamation, States.ide, States.log_complete], temp_transitions)
-
-[recognized, value] = fsm.run('||')
+[recognized, value] = fsm.run(input_string)
 print(recognized)
 print(value)
+
